d9f0e22e63d6c5150413e1455f03bc054593b984
punditjava <andr67890@gmail.com>
2019-09-20 21:57:37 +0300

    Ez

---
+++ firstproga.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+void Func2(int number)
+{
+	register int i = 1;
+	unsigned int mask = ~((~0u) >> 1);
+	for (mask = ~((~0u) >> 1); mask != 0; mask >>= 1, ++i)
+	{
+		putchar(mask & (unsigned int)number ? '1' : '0');
+		if (i % 8 == 0 && mask != 1)
+		{
+			putchar('|');
+			continue;
+		}
+		else 
+		{
+			if (i % 4 == 0)
+				putchar(' ');
+		}
+	}
+}
+int main(void) 
+{ 
+	Func2(10);
+	return 0;
+}
